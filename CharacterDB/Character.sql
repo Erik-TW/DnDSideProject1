f106c7e81ef2de5375b3cca7a4f7dab9c20c1faf
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2020-06-15 19:33:29 +0200

    Add project files.

---
+++ CharacterDB/Character.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Character]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(100) NOT NULL, 
+    [MaxHp] INT NULL, 
+    [AC] INT NOT NULL, 
+    [InitiativeBonus] INT NULL, 
+    [PC] TINYINT NOT NULL, 
+    [Type] NVARCHAR(50) NULL, 
+    [CR] FLOAT NULL
+)
