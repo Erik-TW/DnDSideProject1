6eede8c49ae71b7ad51511bf3e66e6f41ef64af2
Kavilugu <kavilugu@hotmail.com>
2020-08-16 16:44:58 +0200

    Added a rough UI

--- DnDClient/Form1.Designer.cs
+++ DnDClient/Form1.Designer.cs
@@ -28,13 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SearchListBox = new System.Windows.Forms.ListBox();
+            this.EncounterListBox = new System.Windows.Forms.ListBox();
+            this.SearchTextBox = new System.Windows.Forms.TextBox();
+            this.ModifierTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.HealButton = new System.Windows.Forms.Button();
+            this.DamageButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.InitiativeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchListBox
+            // 
+            this.SearchListBox.FormattingEnabled = true;
+            this.SearchListBox.ItemHeight = 15;
+            this.SearchListBox.Location = new System.Drawing.Point(40, 39);
+            this.SearchListBox.Name = "SearchListBox";
+            this.SearchListBox.Size = new System.Drawing.Size(330, 274);
+            this.SearchListBox.TabIndex = 0;
+            this.SearchListBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // EncounterListBox
+            // 
+            this.EncounterListBox.FormattingEnabled = true;
+            this.EncounterListBox.ItemHeight = 15;
+            this.EncounterListBox.Location = new System.Drawing.Point(437, 39);
+            this.EncounterListBox.Name = "EncounterListBox";
+            this.EncounterListBox.Size = new System.Drawing.Size(330, 274);
+            this.EncounterListBox.TabIndex = 0;
+            // 
+            // SearchTextBox
+            // 
+            this.SearchTextBox.Location = new System.Drawing.Point(86, 363);
+            this.SearchTextBox.Name = "SearchTextBox";
+            this.SearchTextBox.Size = new System.Drawing.Size(177, 23);
+            this.SearchTextBox.TabIndex = 1;
+            // 
+            // ModifierTextBox
+            // 
+            this.ModifierTextBox.Location = new System.Drawing.Point(482, 362);
+            this.ModifierTextBox.Name = "ModifierTextBox";
+            this.ModifierTextBox.Size = new System.Drawing.Size(177, 23);
+            this.ModifierTextBox.TabIndex = 1;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(86, 415);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(188, 415);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // HealButton
+            // 
+            this.HealButton.Location = new System.Drawing.Point(482, 415);
+            this.HealButton.Name = "HealButton";
+            this.HealButton.Size = new System.Drawing.Size(75, 23);
+            this.HealButton.TabIndex = 4;
+            this.HealButton.Text = "Heal";
+            this.HealButton.UseVisualStyleBackColor = true;
+            // 
+            // DamageButton
+            // 
+            this.DamageButton.Location = new System.Drawing.Point(588, 415);
+            this.DamageButton.Name = "DamageButton";
+            this.DamageButton.Size = new System.Drawing.Size(75, 23);
+            this.DamageButton.TabIndex = 5;
+            this.DamageButton.Text = "Damage";
+            this.DamageButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(689, 362);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(85, 23);
+            this.RemoveButton.TabIndex = 6;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // InitiativeButton
+            // 
+            this.InitiativeButton.Location = new System.Drawing.Point(689, 415);
+            this.InitiativeButton.Name = "InitiativeButton";
+            this.InitiativeButton.Size = new System.Drawing.Size(85, 23);
+            this.InitiativeButton.TabIndex = 7;
+            this.InitiativeButton.Text = "Set Initiative";
+            this.InitiativeButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.InitiativeButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.DamageButton);
+            this.Controls.Add(this.HealButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.ModifierTextBox);
+            this.Controls.Add(this.SearchTextBox);
+            this.Controls.Add(this.EncounterListBox);
+            this.Controls.Add(this.SearchListBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox SearchListBox;
+        private System.Windows.Forms.ListBox EncounterListBox;
+        private System.Windows.Forms.TextBox SearchTextBox;
+        private System.Windows.Forms.TextBox ModifierTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button HealButton;
+        private System.Windows.Forms.Button DamageButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button InitiativeButton;
     }
 }
 
--- DnDClient/Form1.cs
+++ DnDClient/Form1.cs
@@ -17,5 +17,14 @@ namespace DnDClient
             InitializeComponent();
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
