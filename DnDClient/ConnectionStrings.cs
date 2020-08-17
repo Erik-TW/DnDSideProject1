2c53e5e78dd4c0efaf440cb7d22f94fc8f604de3
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2020-08-17 13:29:57 +0200

    added connection strings

---
+++ DnDClient/ConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace DnDClient
+{
+    class ConnectionStrings
+    {
+        public static string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
--- DnDClient/Program.cs
+++ DnDClient/Program.cs
@@ -18,6 +18,8 @@ namespace DnDClient
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine(ConnectionStrings.GetConnectionString("MonsterManualDB"));
         }
     }
+
 }
