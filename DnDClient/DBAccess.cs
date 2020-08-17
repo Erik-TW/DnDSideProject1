e2556ea642817add1dcf0815794ab531eac4cb0a
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2020-08-17 14:04:48 +0200

    added the database connection class

---
+++ DnDClient/DBAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Dapper;
+
+namespace DnDClient
+{
+    class DBAccess
+    {
+        public string getMonsters()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(ConnectionStrings.GetConnectionString("MonsterManualDb")))
+            {
+                connection.Query("select * from Character;");
+            }
+                return "";
+        }
+    }
+}
--- DnDClient/Program.cs
+++ DnDClient/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dapper;
 
 namespace DnDClient
 {
@@ -14,10 +16,12 @@ namespace DnDClient
         [STAThread]
         static void Main()
         {
+            /* 
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            */
         }
     }
 
